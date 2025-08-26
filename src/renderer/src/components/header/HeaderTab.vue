3de7bf5c0a2721066e3c3c76aa4080e7f3a70fa4
Novout <novout@hotmail.com>
2025-08-26 18:42:11 -0300

    fix(tab): icon position

--- src/renderer/src/components/header/HeaderTab.vue
+++ src/renderer/src/components/header/HeaderTab.vue
@@ -6,15 +6,12 @@
     @click="onLoadTab"
     class="flex items-center gap-2 min-h-10 h-full cursor-pointer overflow-x-auto"
   >
-    <div class="flex justify-center items-center gap-2 w-85% h-full cursor-pointer overflow-x-auto">
-      <IconSpinner v-if="!props.tab.loadedFavicon" class="h-4 w-4" />
-      <img
-        v-else-if="props.tab.icon"
-        class="pl-4"
-        :src="props.tab.icon"
-        alt="icon from target website"
-      />
-      <IconViewTab v-else class="w-4 h-4" />
+    <div class="flex justify-start items-center gap-2 w-85% h-full cursor-pointer overflow-x-auto">
+      <div class="flex items-center pl-4">
+        <IconSpinner v-if="!props.tab.loadedFavicon" class="h-5 w-5" />
+        <img v-else-if="props.tab.icon" :src="props.tab.icon" alt="icon from target website" />
+        <IconViewTab v-else class="w-5 h-5" />
+      </div>
       <IconAudioOn @click="onMutedSound" class="h-5 w-5 cursor-pointer" v-if="audio === 'on'" />
       <IconAudioMuted
         @click="onActiveSound"
