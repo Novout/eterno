a3aa9e07e1f64b9dbb19b3e1a67de43b3cfe67bf
Novout <novout@hotmail.com>
2025-08-28 19:37:37 -0300

    fix(store): remove stores in purge

--- src/renderer/src/use/purge.ts
+++ src/renderer/src/use/purge.ts
@@ -1,21 +1,12 @@
 import { Data } from '@/types'
-import { useHistoryStore } from '@/stores/history'
-import { useNavigatorStore } from '@/stores/navigator'
-import { useOptionsStore } from '@/stores/options'
-import { useProfileStore } from '@/stores/profile'
 
 export const usePurge = () => {
-  const HISTORY = useHistoryStore()
-  const NAVIGATOR = useNavigatorStore()
-  const OPTIONS = useOptionsStore()
-  const PROFILE = useProfileStore()
-
-  const getData = (customData?: Data): Data => {
+  const getData = (customData: Data): Data => {
     let _ = {
-      HISTORY: customData?.HISTORY || HISTORY.$state,
-      NAVIGATOR: customData?.NAVIGATOR || NAVIGATOR.$state,
-      OPTIONS: customData?.OPTIONS || OPTIONS.$state,
-      PROFILE: customData?.PROFILE || PROFILE.$state
+      HISTORY: customData?.HISTORY,
+      NAVIGATOR: customData?.NAVIGATOR,
+      OPTIONS: customData?.OPTIONS,
+      PROFILE: customData?.PROFILE
     } as Data
 
     // intercept data for support older versions
